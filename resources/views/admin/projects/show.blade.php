cddaa86bbdc55f7f2454b916c17f0908cd2dbc4e
Manuel <manuelloverso60@gmail.com>
2024-05-27 14:50:57 +0200

    print technologies on show view

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -19,6 +19,14 @@
                     <p><strong>Type:
                         </strong>{{ $project->type ? $project->type->name : 'There are no types linked to this project' }}
                     </p>
+                    <p><strong>Technologies: </strong>
+                        @forelse ($project->technologies as $technology)
+                            {{-- @dd($technology) --}}
+                            {{ $technology->name }}
+                        @empty
+                            There are no technologies linked
+                        @endforelse
+                    </p>
                     <p><strong>Created: </strong>{{ $project->date }}</p>
                     <a class="btn btn-dark" href="{{ route('admin.projects.edit', $project) }}">Edit</a>
                     @include('partials.delete-modal')
